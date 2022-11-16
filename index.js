bb1b7d0baac4a6bdd542d520eec9a4925f33eb85
David Herring <davidherring@Davids-MBP.home>
2022-11-16 15:31:31 -0600

    setup

---
+++ index.js
@@ -0,0 +1,6 @@
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/intern');
+const Manager = require('./lib/manager')
+const inquirer = require('inquirer');
+const fs = require('fs');
+
